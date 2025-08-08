5efc833818cbf010c1e55ae856671019575ddacc
Tubagus Rifan <tubagusrifan@gmail.com>
2025-08-08 11:21:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Input id="telepho

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-primary/20 text-primary-foreground border-2 border-primary font-bold",
+      "flex h-full w-full items-center justify-center rounded-full bg-primary/20 text-primary font-bold border-2 border-primary",
       className
     )}
     {...props}

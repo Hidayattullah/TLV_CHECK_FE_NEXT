5017b65a248780169129f902188c262a2048e426
Tubagus Rifan <tubagusrifan@gmail.com>
2025-08-11 10:04:19 +0000

    update terbaru untuk tombol

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/80 active:scale-95 disabled:bg-primary/50",
+        default: "btn-primary-fixed active:scale-95",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90 active:scale-95 disabled:bg-destructive/50",
         outline:
